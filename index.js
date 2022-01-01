64773450c584460314e6f9adee9380848cb79f57
Dwight Spencer <dspencer@computekindustries.com>
2022-01-01 17:42:38 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./app";
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.scss";
+
+render(<App />, document.querySelector("#root"));
+
+serviceWorker.unregister();

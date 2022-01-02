7d320557e82d4d1b42b81d185ace531082ea5bf3
Dwight Spencer <dspencer@computekindustries.com>
2022-01-01 18:00:17 -0600

    Create index.js

---
+++ components/navbar/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Button, Media } from "spectre-react";
+
+export default class Banner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: props.backgroundColor || "#000",
+      links: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      links: [
+        { url: "/about", text: "About" },
+        { url: "/topics", text: "Articles" },
+        { url: "/conduct", text: "Conduct" }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <Navbar style={{ background: this.state.background }}>
+        <Navbar.Section>
+          <Navbar.Brand>
+            <Link to="/">
+              <Media.Image
+                style={{ height: 128 }}
+                src="https://pbs.twimg.com/profile_images/583692287537979392/BecmYOgA_400x400.png"
+              />
+            </Link>
+          </Navbar.Brand>
+        </Navbar.Section>
+
+        <Navbar.Section>
+          {this.state.links.map((_) => (
+            <Button link>
+              <Link to={_.url}>{_.text}</Link>
+            </Button>
+          ))}
+        </Navbar.Section>
+      </Navbar>
+    );
+  }
+}
